ece3d8911f614976b499b9e7b540af81cf096f78
Yashaswi Yogeshwara <yashaswi.yogeshwara@gmail.com>
2017-12-07 22:58:35 +0530

    Added basic Search functionality

--- Search.js
+++ Search.js
@@ -4,17 +4,42 @@ function onClientLoad() {
 }
 
 function onYouTubeLoaded() {
-    var request = gapi.client.youtube.search.list({
-        q: 'goomer',
-        type: 'video',
-        part: 'snippet',
-        maxResults:6
-    });
-    request.execute(function (response) {
-        for (var vds in response.items) {
-            createYouTubeFrame(response.items[vds].id["videoId"]);
-        }
-    });
+    createSearchBox();
+}
+
+function createSearchBox() {
+    var searchCtr = document.getElementById('searchContainer');
+    var searchBox = document.createElement('input');
+    searchBox.setAttribute("type", "text");
+    searchBox.setAttribute("id", "searchBox");
+    searchBox.setAttribute("width", 100);
+    searchBox.setAttribute("height", 30);
+    searchCtr.appendChild(searchBox);
+
+    var searchBtn = document.createElement('button');
+    searchBtn.setAttribute("text", "search");
+    searchBtn.setAttribute("width", 80);
+    searchBtn.setAttribute("height", 30);
+    searchBtn.onclick = requestYoutube;
+    searchCtr.appendChild(searchBtn);
+}
+
+
+function requestYoutube(searchTxt) {
+    var searchTxt = document.getElementById("searchBox").value;
+    if (searchTxt) {
+        var request = gapi.client.youtube.search.list({
+            q: searchTxt,
+            type: 'video',
+            part: 'snippet',
+            maxResults: 6
+        });
+        request.execute(function (response) {
+            for (var vds in response.items) {
+                createYouTubeFrame(response.items[vds].id["videoId"]);
+            }
+        });
+    }
 }
 
 function createYouTubeFrame(videoId) {
